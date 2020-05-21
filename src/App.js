bbb7ae12197c3c7769ec6ae3e94e26bc6bacd233
imanahsann <imanahsan@gmail.com>
2020-05-21 15:51:07 -0400

    lifting up state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import {
   BrowserRouter as Router,
   Switch,
@@ -18,6 +19,85 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 library.add(fab);
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      // loading
+      loading: true,
+      // retrieved articles
+      articles: [],
+      //cached articles
+      articleCache: {
+        featured: {},
+        recent: {},
+        reviews: {}
+      },
+      // total pages
+      totalPages: undefined,
+      // current page
+      currentPage: undefined,
+      // category
+      category: undefined,
+      // tag
+      tag: undefined,
+      // active filter
+      filter: undefined
+    }
+  }
+
+  articleRetrieval = (category, tag, page, filter) => {
+    // update state to loading
+    this.setState({
+      loading: true
+    })
+
+    // update url
+    let url = '';
+    if (category || tag) {
+      url = `http://doorinthefloor.net/sadaf/wp-json/wp/v2/posts?${category ? `categories=${category}&` : ''}${tag ? `tags=${tag}&` : ''}${page ? `per_page=12&page=${page}&` : 'per_page=12&'}_embed`;
+    }
+    else {
+      url = `http://doorinthefloor.net/sadaf/wp-json/wp/v2/posts?${page ? `per_page=12&page=${page}&` : ''}_embed`;
+    }
+
+    // check if articles already cached
+    if (this.state.articleCache[filter][page]) {
+      this.setState({
+        articles: this.state.articleCache[filter][page].articles,
+        totalPages: this.state.articleCache[filter][page].totalPages,
+        currentPage: page,
+        category: category,
+        tag: tag,
+        filter: filter,
+        loading: false
+      })
+    }
+
+    // if not, retreive articles and cache
+    else {
+      let articleCacheUpdate = this.state.articleCache;
+      axios({
+        url: url,
+        method: 'GET',
+        dataType: 'json'
+      }).then((response) => {
+        articleCacheUpdate[filter][page] = {
+          articles: response.data,
+          totalPages: response.headers['x-wp-totalpages']
+        };
+        this.setState({
+          articles: response.data,
+          totalPages: response.headers['x-wp-totalpages'],
+          currentPage: page,
+          category: category,
+          tag: tag,
+          filter: filter,
+          loading: false,
+          articleCache: articleCacheUpdate
+        });
+      })
+    }
+  };
 
   render() {
     return (
@@ -25,10 +105,15 @@ class App extends Component {
         <Router>
           <Header />
             <Switch>
+
               <Route exact path="/" render={(props) => <Writing {...props} category={6} page={1} filter={'featured'} />} />
+
               <Route path="/about" component={About} />
+
               <Route path="/contact" component={Contact} />
+
               <Route path="/writing" render={(props) => <Writing {...props} category={queryString.parse(props.location.search).category} tag={queryString.parse(props.location.search).tag} page={queryString.parse(props.location.search).page} filter={queryString.parse(props.location.search).filter} />} />
+
               <Route component={Four04} />
             </Switch>
           <Footer />
--- src/components/Writing.js
+++ src/components/Writing.js
@@ -1,129 +1,49 @@
 import React, { Component } from 'react';
-import axios from 'axios';
 import {
     Link
 } from 'react-router-dom';
 import Article from './Article';
 
 class Writing extends Component {
-    constructor() {
-        super()
-        this.state = {
-            // loading
-            loading: true,
-            // retrieved articles
-            articles: [],
-            //cached articles
-            articleCache: {
-                featured: {},
-                recent: {},
-                reviews: {}
-            },
-            // total pages
-            totalPages: undefined,
-            // current page
-            currentPage: undefined,
-            // category
-            category: undefined,
-            // tag
-            tag: undefined,
-            // active filter
-            filter: undefined
-        }
-    }
 
     componentDidMount() {
-        this.articleRetrieval(this.props.category, this.props.tag, this.props.page, this.props.filter);
+        this.props.articleRetrieval(this.props.category, this.props.tag, this.props.page, this.props.filter);
     }
 
     componentDidUpdate(prevProps) {
         if (prevProps.location.key !== this.props.location.key) {
-            this.articleRetrieval(this.props.category, this.props.tag, this.props.page, this.props.filter);
+            this.props.articleRetrieval(this.props.category, this.props.tag, this.props.page, this.props.filter);
         }
     }
-
-    articleRetrieval = (category, tag, page, filter) => {
-        // update state to loading
-        this.setState({
-            loading: true
-        })
-
-        // update url
-        let url = '';
-        if (category || tag) {
-            url = `http://doorinthefloor.net/sadaf/wp-json/wp/v2/posts?${category ? `categories=${category}&` : ''}${tag ? `tags=${tag}&` : ''}${page ? `per_page=12&page=${page}&` : 'per_page=12&'}_embed`;
-        }
-        else {
-            url = `http://doorinthefloor.net/sadaf/wp-json/wp/v2/posts?${page ? `per_page=12&page=${page}&` : ''}_embed`;
-        }
-
-        // check if articles already cached
-        if ( this.state.articleCache[filter][page] ) {
-            this.setState ({
-                articles: this.state.articleCache[filter][page].articles,
-                totalPages: this.state.articleCache[filter][page].totalPages,
-                currentPage: page,
-                category: category,
-                tag: tag,
-                filter: filter,
-                loading: false
-            })
-        }
-
-        // if not, retreive articles and cache
-        else {
-            let articleCacheUpdate = this.state.articleCache;
-            axios({
-                url: url,
-                method: 'GET',
-                dataType: 'json'
-            }).then((response) => {
-                articleCacheUpdate[filter][page] = {
-                    articles: response.data,
-                    totalPages: response.headers['x-wp-totalpages']
-                };
-                this.setState({
-                    articles: response.data,
-                    totalPages: response.headers['x-wp-totalpages'],
-                    currentPage: page,
-                    category: category,
-                    tag: tag,
-                    filter: filter,
-                    loading: false,
-                    articleCache: articleCacheUpdate
-                });
-            })
-        }
-    };
 
     render() {
         return (
             <div className="wrapper">
                 <div className="articleNav">
                     <ul>
-                        <Link to="/"><li className={(this.state.filter === 'featured' ? 'active' : '')}>Featured</li></Link>
-                        <Link to="/writing?page=1&filter=recent"><li className={(this.state.filter === 'recent' ? 'active' : '')}>Recent</li></Link>
-                        <Link to="/writing?tag=42&page=1&filter=reviews"><li className={(this.state.filter === 'reviews' ? 'active' : '')}>Reviews</li></Link>
+                        <Link to="/"><li className={(this.props.filter === 'featured' ? 'active' : '')}>Featured</li></Link>
+                        <Link to="/writing?page=1&filter=recent"><li className={(this.props.filter === 'recent' ? 'active' : '')}>Recent</li></Link>
+                        <Link to="/writing?tag=42&page=1&filter=reviews"><li className={(this.props.filter === 'reviews' ? 'active' : '')}>Reviews</li></Link>
                         <Link><li>News - update</li></Link>
                     </ul>
                 </div>
                 <div className="loading-container">
-                    <div className={this.state.loading ? 'loader loading' : 'loader'}>
+                    <div className={this.props.loading ? 'loader loading' : 'loader'}>
                         <p>LOADING</p>
                     </div>
                 </div>
-                <div className={this.state.loading ? 'articleGrid loading' : 'articleGrid'}>
-                    {this.state.articles.map((article) => {
+                <div className={this.props.loading ? 'articleGrid loading' : 'articleGrid'}>
+                    {this.props.articles.map((article) => {
                         return <Article key={article.id} title={article.content.rendered} link={article.Link} publication={article.Publication} image={article._embedded["wp:featuredmedia"] ? `${article._embedded["wp:featuredmedia"]["0"].source_url}` : ''} date={article.date} />
                     })}
                 </div>
                 <div className="pagination">
                     <ul>
-                        <Link to={`/writing?${this.state.category ? `category=${this.state.category}&` : ''}${this.state.tag ? `tag=${this.state.tag}&` : ''}page=${parseInt(this.state.currentPage) - 1}&filter=${this.state.filter}`} className={(parseInt(this.state.currentPage) !== 1) ? 'back' : 'invisible back'} ><li className="back">{String.fromCharCode(171) + ' Previous'}</li></Link>
+                        <Link to={`/writing?${this.props.category ? `category=${this.props.category}&` : ''}${this.props.tag ? `tag=${this.props.tag}&` : ''}page=${parseInt(this.props.currentPage) - 1}&filter=${this.props.filter}`} className={(parseInt(this.props.currentPage) !== 1) ? 'back' : 'invisible back'} ><li className="back">{String.fromCharCode(171) + ' Previous'}</li></Link>
 
-                        <li className="page">{this.state.currentPage} of <Link to={`/writing?${this.state.category ? `category=${this.state.category}&` : ''}${this.state.tag ? `tag=${this.state.tag}&` : ''}page=${this.state.totalPages}&filter=${this.state.filter}`}>{this.state.totalPages}</Link></li>
+                        <li className="page">{this.props.currentPage} of <Link to={`/writing?${this.props.category ? `category=${this.props.category}&` : ''}${this.props.tag ? `tag=${this.props.tag}&` : ''}page=${this.props.totalPages}&filter=${this.props.filter}`}>{this.props.totalPages}</Link></li>
 
-                        <Link to={`/writing?${this.state.category ? `category=${this.state.category}&` : ''}${this.state.tag ? `tag=${this.state.tag}&` : ''}page=${parseInt(this.state.currentPage) + 1}&filter=${this.state.filter}`} className={(parseInt(this.state.currentPage) < parseInt(this.state.totalPages)) ? 'next' : 'invisible next' } ><li>{'Next ' + String.fromCharCode(187)}</li></Link>
+                        <Link to={`/writing?${this.props.category ? `category=${this.props.category}&` : ''}${this.props.tag ? `tag=${this.props.tag}&` : ''}page=${parseInt(this.props.currentPage) + 1}&filter=${this.props.filter}`} className={(parseInt(this.props.currentPage) < parseInt(this.props.totalPages)) ? 'next' : 'invisible next' } ><li>{'Next ' + String.fromCharCode(187)}</li></Link>
                     </ul>
                 </div>
             </div>
